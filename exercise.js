89e1a4c77c3d0cd5ebfd89183d83383e27815e53
digitsmyanmar72 <naywinaung522522@gmail.com>
2025-04-10 11:45:38 +0630

    10.4.2025

--- exercise.js
+++ exercise.js
@@ -1,37 +1,63 @@
-// set up dependencies
+//set up dependencies
 const express = require('express');
 const mysql = require('mysql2');
 const JWT = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const app = require('express');
 
-// to parse incoming json data from http request 
+//middleware to parse json data from http request
+app.use(express.json());
 
-app.request(express.json());
-
-// create mysql connection pool
+//create mysql connection pool
 const pool = mysql.createPool({
     host : 'localhost',
-    user : 'root',
-    password : '',
+    user:'root',
+    password:'',
     database:'mydatabase',
     waitForConnections:true,
-    connectionLimit : 10,
-    queueLimit : 0 ,
-})
+    connectionLimit:10,
+    queueLimit:0,
+});
 
-// secret key for jwt token
-const JWT_SECRET ='';
-
-//middleware to authenticate token
-function authenticateToken (req,res,next)
-{
+//Secret Key for JWT
+const JWT_SECRET='';
 
 
 
+//middleware to authenticate JWT token from http request
+function authenticateToken(req,res,next){
+  //token extraction
+  const authHeader = req.authHeader['authorization'];
+  const token = authHeader && authHeader.split('')[1];
+
+  //check token
+  if(!token){
+    return res.status(401).json({error:'access denied.no token provided'
+    })
+  };
+
+  //verify jwt token
+   jwt.verify (token,JWT_SECRET,(err,user)=>{
+       if(error){
+        return res.status(301).json({error:'invalid token'});
+       };
+
+    //attach user datat to the request
+    req.user = user ;
+
+    //proceed to the next middleware
+    next();
+
+    });
+       
+    
+
+  
+  
+
+}
 
 
 
 
     
-}
